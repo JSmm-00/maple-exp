2e0b45eb50c2c5a3eeca1afb40fa8b3d0e260675
JSmm-00 <wjdwlstn3000@gmail.com>
2026-07-23 01:37:50 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * ★ 여기 한 줄만 고치면 모든 페이지(메인·에픽던전)에 적용됩니다 ★
+ *
+ * Cloudflare Worker를 배포한 뒤 받은 주소를 아래에 붙여넣으세요.
+ * 예: window.MAPLE_PROXY_URL = "https://maple-proxy.mynickname.workers.dev";
+ *
+ * 주소를 넣기 전까지는 캐릭터 검색이 "주소를 먼저 설정하세요" 라고 안내합니다.
+ */
+window.MAPLE_PROXY_URL = "";
